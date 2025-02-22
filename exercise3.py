4d83a098fe3bae9ee1cbd0a19ca1dad0b79e47c2
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-22 08:32:18 -0500

    Michael H

--- exercise3.py
+++ exercise3.py
@@ -1,40 +1,32 @@
-#  Zion Needham (Ultimate-Shoe)
-#  W25 COP2002.0M1.
-#  02/06/2025
-#  MAC Manufacturer Identification Program
-#  Simple Python program to give a user the manufacturer name based on the MAC address they input
+# Michael Harrison
+# COP2002.OM1
+# Exercise 3 - MAC Manufacturer Program
 
-def main():
-    # Introduction to the program and prompts user for MAC  address .
-    print("MAC Manufacturer Program \n-------------------------\n")
-    prompt = "Enter the first 6 hex values of the MAC address (format as XX:XX:XX): "
-    user_MAC = input(prompt)
+print (" Mac Manufacturer Program")
+print (" -----------------------\n")
 
-    # Basic if/elif to check the user's input MAC address with the proper manufacturer.
-    if user_MAC == "00:00:17":
-        manufacturer = "Oracle"
 
-    elif user_MAC == "00:07:E9":
-        manufacturer = "Intel Corporation"
 
-    elif user_MAC == "04:27:28":
-        manufacturer = "Microsoft Corporation"
+mac = input ("Enter the first six Hex digits seperated by colons in the format XX:XX:XX:  ")
+if mac == "00:00:17":
+    print ("Oracle")
 
-    elif user_MAC == "04:26:65":
-        manufacturer = "Apple, Inc."
+elif mac == "00:07:E9":
+    print ("Intel Corporation")
 
-    elif user_MAC == "04:33:89":
-        manufacturer = "Huawei Technologies Co., Ltd"
+elif mac == "04:27:28":
+    print ("Microsoft Corporation")
 
-    elif user_MAC == "00:00:0C":
-        manufacturer = "Cisco Systems, Inc"
+elif mac == "04:26:65":
+    print ("Apple, Inc.")
 
-    else:   # This will catch invalid values or values not found.
-        manufacturer = "Unknown"
+elif mac == "04:33:89":
+    print ("Huawei Technologies Co.,Ltd")
 
-    # Output result telling the user the manufacturer name for their MAC address.
-    print(f"For {user_MAC} the MAC manufacturer is {manufacturer}.")
+elif mac == "00:00:0C":
+    print ("Cisco Systems, Inc")
 
-# Calling the main function.
-if __name__ == "__main__":
-    main()
+else:
+    print ("unknown")
+
+#End Program
