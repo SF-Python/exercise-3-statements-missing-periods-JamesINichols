8eb4f138c45932df3db6f7635237f0537dd54690
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-22 08:24:54 -0500

    Morgan R

--- exercise3.py
+++ exercise3.py
@@ -1,32 +1,27 @@
-# Gianna Walter
-# Course ID: COP2002 and section #0M1
-# 2/05/2025
-# Exercise 3
-# If Statements
+#Morgan Ray (morgan-rayCOP2002)
+#COP2002.0M1
+#February 6,2025
+#Exercise 3
+#If Statements
+#
+#This Program will take user input for a MAC address and which Tech Organization that it identifies
+ 
+#Get user input
 
-# Created main function
-def main():
-    # created list of manufacturer codes
-    manfact_codes = [("00:00:17","Oracle"), ("00:07:E9","Intel Corporation"),("04:27:28","Microsoft Corporation"),("04:26:65","Apple, Inc."),("04:33:89","Huawei Technologies Co. Ltd"),("00:00:0C","Cisco Systems, Inc")]
+oui=input("What are the first 6 hexadecimal digits of your MAC address? ")
 
-    # Asked for user input
-    user_input = input("Enter the first 6 hex digits of the MAC address (XX:XX:XX): ")
+#Process input and give an answer
 
-    # defining found variable
-    found=False
-    # looping through the list
-    for hex_dig, maufact in manfact_codes:
-        # checking if user input is in the list
-        if user_input==hex_dig:
-            # printing the manufacturer if found in the list
-            print(f"The manufacturer is {maufact}")
-            # setting found to true
-            found = True
-            break
-    # printing unknown if not found
-    if not found:
-        print("Manufacturer: Unknown")
-        
-# Calling main function
-if __name__=="__main__":
-    main()
+if oui == "00:00:17":
+    print("This is the OUI for Oracle")
+elif oui == "00:07:E9":
+    print("This is the OUI for Intel Corporation")
+elif oui == "04:27:28":
+    print("This is the OUI for Microsoft Corporation")
+elif oui == "04:26:65":
+    print("This is the OUI for Apple, Inc.")
+elif oui == "04:33:89":
+    print("This is the OUI for Huawei Technologies Co.,Ltd")   
+elif oui == "00:00:0C":
+    print("This is the OUI for Cisco Systems, Inc")
+else: print("Invalid OUI")
