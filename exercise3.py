ed9801ff15f458a99f11cc35f78411fb63f89280
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-22 08:19:05 -0500

    Gianna

--- exercise3.py
+++ exercise3.py
@@ -1,31 +1,32 @@
-# Chris Fillyaw (ChrisFilyaw1)
-# COP2002-OM1
-# February ,2025
+# Gianna Walter
+# Course ID: COP2002 and section #0M1
+# 2/05/2025
 # Exercise 3
-# A program that determines who the manufacturer is for a NIC card using the first 6 hex digits
+# If Statements
 
+# Created main function
 def main():
-    print("MAC Manufacturer Program\n------------------------\n") # Print the title
-    user_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ") # Get user input
+    # created list of manufacturer codes
+    manfact_codes = [("00:00:17","Oracle"), ("00:07:E9","Intel Corporation"),("04:27:28","Microsoft Corporation"),("04:26:65","Apple, Inc."),("04:33:89","Huawei Technologies Co. Ltd"),("00:00:0C","Cisco Systems, Inc")]
 
-   # Define the manufacturer based on the user input
-    if user_input == "00:00:17": 
-        manufacturer = "Oracle"
-    elif user_input == "00:07:E9":
-        manufacturer = "Intel Corporation"
-    elif user_input == "04:27:28":
-        manufacturer = "Microsoft Corporation"
-    elif user_input == "04:26:65":
-        manufacturer = "Apple, Inc"
-    elif user_input == "04:33:89":
-        manufacturer = "Huawei Technologies Co.,Ltd"
-    elif user_input == "00:00:0C":
-        manufacturer = "Cisco Systems, Inc"
-    else:
-        manufacturer = "<Not valid value or not found> Unknown"
-    
-    
-    print(f"For {user_input} the MAC manufacturer is {manufacturer}.") # Print the manufacturer
+    # Asked for user input
+    user_input = input("Enter the first 6 hex digits of the MAC address (XX:XX:XX): ")
 
-if __name__ == "__main__": # Run the main function
+    # defining found variable
+    found=False
+    # looping through the list
+    for hex_dig, maufact in manfact_codes:
+        # checking if user input is in the list
+        if user_input==hex_dig:
+            # printing the manufacturer if found in the list
+            print(f"The manufacturer is {maufact}")
+            # setting found to true
+            found = True
+            break
+    # printing unknown if not found
+    if not found:
+        print("Manufacturer: Unknown")
+        
+# Calling main function
+if __name__=="__main__":
     main()
