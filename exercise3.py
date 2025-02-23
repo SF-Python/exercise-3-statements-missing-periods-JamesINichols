d0d0cb20a6ad3da381cb0a2a658ef9dfa66090f3
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-22 22:32:03 -0500

    Rohan C

--- exercise3.py
+++ exercise3.py
@@ -1,31 +1,28 @@
-# Kyler Cole
-# COP2002.0M1
-# 12 February 2025
-# MAC Manufacturer Resolution to Developer Company
-# This program will run to allow the end user to input the first six MAC Address Hexadecimal digits of their NIC to resolve it to the manufacturer company.
+# Rohan Caballero (Gigantopithecus1987)
+# COP2002-0M1
+# January 16, 2025
+# Exercise 3
+# Program that returns the MAC manufacturer based on the MAC address
 
-# Program name
-print('MAC Manufacturer Program')
-print('------------------------')
+#Lists for both the input and output
+Ouput=["Oracle", "Intel Corporation", "Microsoft Corporation", "Apple, Inc.", "Huawei Technologies Co.,Ltd", "Cisco Systems, Inc", "Unknown"]
+Input=["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C",]
 
-# Dialogue for user to read and type a response to
-nicMAC = input("\nEnter the first 6 hex values of the MAC address of your NIC (format as XX:XX:XX): ")
+#Input measure
+prompt=input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
 
-# Block of code that will give the user an answer based on their input
-if nicMAC == '00:00:17':
-    manufacturer = 'Oracle'
-elif nicMAC == '00:07:E9':
-    manufacturer = 'Intel Corporation'
-elif nicMAC == '04:27:28':
-    manufacturer = 'Microsoft Corporation'
-elif nicMAC == '04:26:65':
-    manufacturer = 'Apple, Inc.'
-elif nicMAC == '04:33:89':
-    manufacturer = 'Huawei Technologies Co.,Ltd'
-elif nicMAC == '00:00:0C':
-    manufacturer = 'Cisco Systems, Inc.'
+#Input/Output chain
+if prompt==Input[0]:
+    print("For",prompt, "the MAC manufacturer is", Ouput[0])
+elif prompt==Input[1]:
+    print("For",prompt, "the MAC manufacturer is", Ouput[1])
+elif prompt==Input[2]:
+    print("For",prompt, "the MAC manufacturer is", Ouput[2])
+elif prompt==Input[3]:
+    print("For",prompt, "the MAC manufacturer is", Ouput[3])
+elif prompt==Input[4]:
+    print("For",prompt, "the MAC manufacturer is", Ouput[4])
+elif prompt==Input[5]:
+    print("For",prompt, "the MAC manufacturer is", Ouput[5])
 else:
-    manufacturer = 'Unknown'
-
-# End of program statement
-print(f'\nFor {nicMAC} the MAC manufacturer is {manufacturer}.')
+    print("For",prompt, "the MAC manufacturer is", Ouput[-1])
