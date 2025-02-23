d5b7f10e5ebf42bbc89252d583d68718dfa5921b
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-22 22:46:19 -0500

    Logan C

--- exercise3.py
+++ exercise3.py
@@ -1,41 +1,30 @@
-#Dominic Lee
-#02/16/2025
-#COP2002 0M1
-#Exercise 3: If statements
+# Logan Cole ljcole93
+# COP2002.0M1
+# 14Feb25
+# exercise 3
+# A program demonstrating the use of conditions, if statements, and other concepts from chapter 3.
 
-
-print("MAC Manufacturer Program")
-
-print("---------------------")
-
-print() #Blank line
-
+def pull_manu(mac_ID):
+    if mac_ID == "00:00:0C":
+        return "Cisco Systems, Inc"
+    elif mac_ID == "04:33:89":
+        return "Huawei Technologies Co.,Ltd"
+    elif mac_ID == "04:26:65":
+        return "Apple, Inc"
+    elif mac_ID == "04:27:28":
+        return "Microsoft Corporation"
+    elif mac_ID == "00:07:E9":
+        return "Intel Corporation"
+    elif mac_ID == "00:00:17":
+        return "Oracle"
+    else:
+        return "Unknown"
 
 def main():
-
-
-# List of hex codes and manufacturers
-    manufacturers =[
-        ("00:00:17", "Oracle"),
-        ("00:07:E9", "Intel Corporation"),
-        ("04:27:28", "Microsoft Corporation"),
-        ("04:26:65", "Apple, Inc."),
-        ("04:33:89", "Huawei Technologies Co., Ltd"),
-        ("00:00:0C", "Cisco Systems, Inc")]
-    
-# Ask for input
-    hex_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
-    manufacturer = "Unknown"
-
-# Loop through the list to find a match
-    for match, MAC in manufacturers:
-        if hex_input.upper() == match:
-            manufacturer = MAC
-
-
-        # Output the result
-    print(f"For {hex_input} the MAC manufacturer is {manufacturer}.")
+    mac_ID = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX):")
+    manufacturer = pull_manu(mac_ID)
+    print(f"MAC Address first 6: {mac_ID}")
+    print(f"Manufacturer: {manufacturer}")
 
 if __name__ == "__main__":
     main()
-  
