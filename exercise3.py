024508341604017d682e5c4588653ee573492658
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-22 22:50:16 -0500

    Karly

--- exercise3.py
+++ exercise3.py
@@ -1,30 +1,26 @@
-# Logan Cole ljcole93
-# COP2002.0M1
-# 14Feb25
-# exercise 3
-# A program demonstrating the use of conditions, if statements, and other concepts from chapter 3.
-
-def pull_manu(mac_ID):
-    if mac_ID == "00:00:0C":
-        return "Cisco Systems, Inc"
-    elif mac_ID == "04:33:89":
-        return "Huawei Technologies Co.,Ltd"
-    elif mac_ID == "04:26:65":
-        return "Apple, Inc"
-    elif mac_ID == "04:27:28":
-        return "Microsoft Corporation"
-    elif mac_ID == "00:07:E9":
-        return "Intel Corporation"
-    elif mac_ID == "00:00:17":
-        return "Oracle"
-    else:
-        return "Unknown"
-
 def main():
-    mac_ID = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX):")
-    manufacturer = pull_manu(mac_ID)
-    print(f"MAC Address first 6: {mac_ID}")
-    print(f"Manufacturer: {manufacturer}")
+    manufacturers = [
+        ("00:00:17", "Oracle"),
+        ("00:07:E9", "Intel Corporation"),
+        ("04:27:28", "Microsoft Corporation"),
+        ("04:26:65", "Apple, Inc."),
+        ("04:33:89", "Huawei Technologies Co.,Ltd"),
+        ("00:00:0C", "Cisco Systems, Inc")
+    ]
+
+    print("           MAC Manufacturer Program")
+    print("------------------------")
+    print()
+
+    user_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+    
+    manufacturer_name = "Unknown"
+    for code, name in manufacturers:
+        if user_input.upper() == code:
+            manufacturer_name = name
+            break
+
+    print(f"For {user_input} the MAC manufacturer is {manufacturer_name}.")
 
 if __name__ == "__main__":
     main()
