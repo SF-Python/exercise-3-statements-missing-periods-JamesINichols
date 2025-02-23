422f2423a829284e78d9b7994b5e0dce4d624301
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-22 22:52:26 -0500

    Devonta

--- exercise3.py
+++ exercise3.py
@@ -1,26 +1,29 @@
-def main():
-    manufacturers = [
-        ("00:00:17", "Oracle"),
-        ("00:07:E9", "Intel Corporation"),
-        ("04:27:28", "Microsoft Corporation"),
-        ("04:26:65", "Apple, Inc."),
-        ("04:33:89", "Huawei Technologies Co.,Ltd"),
-        ("00:00:0C", "Cisco Systems, Inc")
-    ]
+#Devonta Thurman
+#COP2002.OM1
+#Feb 16, 2025
+#Exercise 3
+#Using If-Statements
 
-    print("           MAC Manufacturer Program")
-    print("------------------------")
-    print()
+print("Enter the first 6 hex values of the MAC address (format as XX:XX:XX):")
 
-    user_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
-    
-    manufacturer_name = "Unknown"
-    for code, name in manufacturers:
-        if user_input.upper() == code:
-            manufacturer_name = name
-            break
+Manufacturer = [f"Oracle", "Intel Corporation", "Microsoft Corporation", "Apple, Inc.", "Huawei Technologies Co.,Ltd", "Cisco Systems, Inc"]
 
-    print(f"For {user_input} the MAC manufacturer is {manufacturer_name}.")
+Hex_Digits = [f"00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C"]
 
-if __name__ == "__main__":
-    main()
+answer = input()
+
+if answer in Hex_Digits:
+    if answer == Hex_Digits[0]:
+        print(f"For {answer} the MAC manufacturer is {Manufacturer[0]}")
+    if answer == Hex_Digits[1]:
+        print(f"For {answer} the MAC manufacturer is {Manufacturer[1]}")
+    if answer == Hex_Digits[2]:
+        print(f"For {answer} the MAC manufacturer is {Manufacturer[2]}")
+    if answer == Hex_Digits[3]:
+        print(f"For {answer} the MAC manufacturer is {Manufacturer[3]}")
+    if answer == Hex_Digits[4]:
+        print(f"For {answer} the MAC manufacturer is {Manufacturer[4]}")
+    if answer == Hex_Digits[5]:
+        print(f"For {answer} the MAC manufacturer is {Manufacturer[5]}")
+else:
+    print("Unknown")
