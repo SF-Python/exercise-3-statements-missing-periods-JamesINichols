627a19ea2cf03d0087f1ae6cc6fe5fc5d060b80c
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-22 22:29:26 -0500

    Kyler

--- exercise3.py
+++ exercise3.py
@@ -1,31 +1,31 @@
-print("Hello, World!")
-def main():
-    # Display the title of the program
-    print("MAC Manufacturer Program")
-    print("------------------------")
-   
-    # Prompt the user for input in the specified format
-    mac_address = input("\nEnter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+# Kyler Cole
+# COP2002.0M1
+# 12 February 2025
+# MAC Manufacturer Resolution to Developer Company
+# This program will run to allow the end user to input the first six MAC Address Hexadecimal digits of their NIC to resolve it to the manufacturer company.
 
-    # Determine the manufacturer based on the input
-    if mac_address == "00:00:17":
-        manufacturer = "Oracle"
-    elif mac_address == "00:07:E9":
-        manufacturer = "Intel Corporation"
-    elif mac_address == "04:27:28":
-        manufacturer = "Microsoft Corporation"
-    elif mac_address == "04:26:65":
-        manufacturer = "Apple, Inc."
-    elif mac_address == "04:33:89":
-        manufacturer = "Huawei Technologies Co.,Ltd"
-    elif mac_address == "00:00:0C":
-        manufacturer = "Cisco Systems, Inc"
-    else:
-        manufacturer = "Unknown"
-   
-    # Output the result
-    print(f"\nFor {mac_address} the MAC manufacturer is {manufacturer}.\n")
+# Program name
+print('MAC Manufacturer Program')
+print('------------------------')
 
-# Call the main function
-if __name__ == "__main__":
-    main()
+# Dialogue for user to read and type a response to
+nicMAC = input("\nEnter the first 6 hex values of the MAC address of your NIC (format as XX:XX:XX): ")
+
+# Block of code that will give the user an answer based on their input
+if nicMAC == '00:00:17':
+    manufacturer = 'Oracle'
+elif nicMAC == '00:07:E9':
+    manufacturer = 'Intel Corporation'
+elif nicMAC == '04:27:28':
+    manufacturer = 'Microsoft Corporation'
+elif nicMAC == '04:26:65':
+    manufacturer = 'Apple, Inc.'
+elif nicMAC == '04:33:89':
+    manufacturer = 'Huawei Technologies Co.,Ltd'
+elif nicMAC == '00:00:0C':
+    manufacturer = 'Cisco Systems, Inc.'
+else:
+    manufacturer = 'Unknown'
+
+# End of program statement
+print(f'\nFor {nicMAC} the MAC manufacturer is {manufacturer}.')
