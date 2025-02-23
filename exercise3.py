026bd11d5cb90f8203ca5f0373b3d98d6ad74b3b
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-22 22:56:05 -0500

    Lexi

--- exercise3.py
+++ exercise3.py
@@ -1,29 +1,44 @@
-#Devonta Thurman
-#COP2002.OM1
-#Feb 16, 2025
-#Exercise 3
-#Using If-Statements
+# Lexi Weaver (AceOfBlades520)
+# COP2002 OM1
+# 02/04/2025
+# Exercise 3
 
-print("Enter the first 6 hex values of the MAC address (format as XX:XX:XX):")
+# Define a dictionary to map the first 6 hex digits to the corresponding manufacturers
+mac_manufacturers = {
+    "00:00:17": "Oracle",
+    "00:07:E9": "Intel Corporation",
+    "04:27:28": "Microsoft Corporation",
+    "04:26:65": "Apple, Inc.",
+    "04:33:89": "Huawei Technologies Co.,Ltd",
+    "00:00:0C": "Cisco Systems, Inc",
+}
 
-Manufacturer = [f"Oracle", "Intel Corporation", "Microsoft Corporation", "Apple, Inc.", "Huawei Technologies Co.,Ltd", "Cisco Systems, Inc"]
+# Define a function to look up the manufacturer by the given hex digits
+def get_manufacturer(hex_input):
+    hex_values = hex_input.split(":")
+    if len(hex_values) == 3:
+        manufacturer_key = ":".join(hex_values).lower()
+        if manufacturer_key in mac_manufacturers:
+            return mac_manufacturers[manufacturer_key]
+        else:
+            return "Unknown"
+    else:
+        return "Invalid format. Please use the correct format: XX:XX:XX"
 
-Hex_Digits = [f"00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C"]
+# Main program to interact with the user
+def main():
+    print("""
+           MAC Manufacturer Program
+------------------------
+""")
+    while True:
+        hex_input = input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+        manufacturer = get_manufacturer(hex_input)
+        if manufacturer == "Invalid format. Please use the correct format: XX:XX:XX":
+            continue
+        print(f"For {hex_input} the MAC manufacturer is {manufacturer}.")
+        break
 
-answer = input()
-
-if answer in Hex_Digits:
-    if answer == Hex_Digits[0]:
-        print(f"For {answer} the MAC manufacturer is {Manufacturer[0]}")
-    if answer == Hex_Digits[1]:
-        print(f"For {answer} the MAC manufacturer is {Manufacturer[1]}")
-    if answer == Hex_Digits[2]:
-        print(f"For {answer} the MAC manufacturer is {Manufacturer[2]}")
-    if answer == Hex_Digits[3]:
-        print(f"For {answer} the MAC manufacturer is {Manufacturer[3]}")
-    if answer == Hex_Digits[4]:
-        print(f"For {answer} the MAC manufacturer is {Manufacturer[4]}")
-    if answer == Hex_Digits[5]:
-        print(f"For {answer} the MAC manufacturer is {Manufacturer[5]}")
-else:
-    print("Unknown")
+# Execute the main function
+if __name__ == "__main__":
+    main()
