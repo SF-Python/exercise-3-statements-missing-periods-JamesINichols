03052ecb1ca30b24c4f802ec6d8e344e305277c3
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-22 22:37:43 -0500

    Collin B

--- exercise3.py
+++ exercise3.py
@@ -1,28 +1,42 @@
-# Rohan Caballero (Gigantopithecus1987)
-# COP2002-0M1
-# January 16, 2025
+# Collin Barnes (Coin117)
+# COP2002.0M1
+# 2-16-2025
 # Exercise 3
-# Program that returns the MAC manufacturer based on the MAC address
+# Program to lookup MAC Manufacturers
 
-#Lists for both the input and output
-Ouput=["Oracle", "Intel Corporation", "Microsoft Corporation", "Apple, Inc.", "Huawei Technologies Co.,Ltd", "Cisco Systems, Inc", "Unknown"]
-Input=["00:00:17", "00:07:E9", "04:27:28", "04:26:65", "04:33:89", "00:00:0C",]
+def get_manufacturer(mac_prefixes, manufacturers, mac_input):
+    for i in range(len(mac_prefixes)):
+        if mac_input.upper() == mac_prefixes[i]:
+            return manufacturers[i]
+    return "Unknown"
 
-#Input measure
-prompt=input("Enter the first 6 hex values of the MAC address (format as XX:XX:XX): ")
+def main():
+    print("           MAC Manufacturer Lookup")
+    print("------------------------")
+    print()
+    
+    mac_prefixes = [
+        "00:00:17",
+        "00:07:E9",
+        "04:27:28",
+        "04:26:65",
+        "04:33:89",
+        "00:00:0C"
+    ]
+    
+    manufacturers = [
+        "Oracle",
+        "Intel Corporation",
+        "Microsoft Corporation",
+        "Apple, Inc.",
+        "Huawei Technologies Co.,Ltd",
+        "Cisco Systems, Inc"
+    ]
+    
+    mac_input = input("Enter the first 6 hex values of the MAC address you wish to find (format as XX:XX:XX): ")
+    manufacturer = get_manufacturer(mac_prefixes, manufacturers, mac_input)
+    
+    print(f"For {mac_input} the MAC manufacturer is {manufacturer}.")
 
-#Input/Output chain
-if prompt==Input[0]:
-    print("For",prompt, "the MAC manufacturer is", Ouput[0])
-elif prompt==Input[1]:
-    print("For",prompt, "the MAC manufacturer is", Ouput[1])
-elif prompt==Input[2]:
-    print("For",prompt, "the MAC manufacturer is", Ouput[2])
-elif prompt==Input[3]:
-    print("For",prompt, "the MAC manufacturer is", Ouput[3])
-elif prompt==Input[4]:
-    print("For",prompt, "the MAC manufacturer is", Ouput[4])
-elif prompt==Input[5]:
-    print("For",prompt, "the MAC manufacturer is", Ouput[5])
-else:
-    print("For",prompt, "the MAC manufacturer is", Ouput[-1])
+if __name__ == "__main__":
+    main()
