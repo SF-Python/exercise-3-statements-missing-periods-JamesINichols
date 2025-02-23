602a9f5184e2812ee99b50c59b299f0e564f363a
James Nichols <106408108+JamesINichols@users.noreply.github.com>
2025-02-22 22:21:10 -0500

    John P

--- exercise3.py
+++ exercise3.py
@@ -11,49 +11,42 @@ def main():
     print("-"*23, "\n")
 
 
-# initializing boolean to be used in while loop allowing program 
-# to restart if invalid input is given from the user
-    restartFlag = True
-
-# while loop to restart program as described in previous comment
-    while(restartFlag == True):
-
 # prompting user for input
-        userIn = input("Type the first six hex digits of the MAC address (format as XX:XX:XX): ")
+    userIn = input("Type the first six hex digits of the MAC address (format as XX:XX:XX): ")
 
 
 # each if/elif statement below returns the manufacturer from known values
 # if any branch prior to the else statement is entered, restart flag is flipped
-        if(userIn == "00:00:17"):
-            print("For 00:00:17 the manufacurer is Oracle.")
-            restartFlag = False
+    if(userIn == "00:00:17"):
+        print("For 00:00:17 the manufacurer is Oracle.")
+        restartFlag = False
 
-        elif(userIn == "00:07:E9"):
-             print("For 00:07:E9 the manufacturer is Intel Coprporation.")
-             restartFlag = False
+    elif(userIn == "00:07:E9"):
+         print("For 00:07:E9 the manufacturer is Intel Coprporation.")
+         restartFlag = False
 
-        elif(userIn == "04:27:28"):
-             print("For 04:27:28 the manufacturer is Microsoft Coroporation.")
-             restartFlag = False
+    elif(userIn == "04:27:28"):
+         print("For 04:27:28 the manufacturer is Microsoft Coroporation.")
+         restartFlag = False
 
-        elif(userIn == "04:26:65"):
-             print("For 04:26:65 the manufacturer is Apple Inc.")
-             restartFlag = False
+    elif(userIn == "04:26:65"):
+         print("For 04:26:65 the manufacturer is Apple Inc.")
+         restartFlag = False
 
-        elif(userIn == "04:33:89"):
-             print("For 04:33:89 the manufacturer is Huawei Technologies Co.,Ltd")
-             restartFlag = False
+    elif(userIn == "04:33:89"):
+         print("For 04:33:89 the manufacturer is Huawei Technologies Co.,Ltd")
+         restartFlag = False
 
-        elif(userIn == "00:00:0C"):
-             print("For 00:00:0C the manufacturer is Cisco Systems, Inc.")
-             restartFlag = False
+    elif(userIn == "00:00:0C"):
+         print("For 00:00:0C the manufacturer is Cisco Systems, Inc.")
+         restartFlag = False
 
 # else statement below tells user thier input is no good;
 # re-assigning restartFlag just for clarity that entering 
 # this branch indicates failure to find manufacturer from input
-        else:
-             print("Input not recognized/unknown manufacturer.  Plase try again.", "\n")
-             restartFlag = True
+    else:
+         print("Input not recognized/unknown manufacturer.  Plase try again.", "\n")
+         restartFlag = True
         
 if(__name__=="__main__"):
     main()
